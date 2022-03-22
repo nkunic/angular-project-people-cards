8aeb42d2e5bc11f3f21583049de8a58b3d14d579
nkunic <natasa.kunic@gmail.com>
2022-03-22 18:35:30 +0100

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { PeopleCardsModule } from './people-cards/people-cards.module';
 import { AppComponent } from './app.component';
-import {
-  FontAwesomeModule,
-  FaIconLibrary,
-} from '@fortawesome/angular-fontawesome';
-import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
-import {
-  faSquare as farSquare,
-  faCheckSquare as farCheckSquare,
-} from '@fortawesome/free-regular-svg-icons';
-import {
-  faStackOverflow,
-  faGithub,
-  faMedium,
-} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, PeopleCardsModule, FontAwesomeModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-  constructor(library: FaIconLibrary) {
-    library.addIcons(
-      faSquare,
-      faCheckSquare,
-      farSquare,
-      farCheckSquare,
-      faStackOverflow,
-      faGithub,
-      faMedium
-    );
-  }
-}
+export class AppModule {}
--- src/app/people-cards/people-cards.module.ts
+++ src/app/people-cards/people-cards.module.ts
@@ -3,16 +3,33 @@ import { CommonModule } from '@angular/common';
 import { PeopleCardContainerComponent } from './container/people-card-container.component';
 import { PeopleCardComponent } from './components/people-card/people-card.component';
 
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSquare as farSquare,
+  faCheckSquare as farCheckSquare,
+} from '@fortawesome/free-regular-svg-icons';
+import {
+  faStackOverflow,
+  faGithub,
+  faMedium,
+} from '@fortawesome/free-brands-svg-icons';
+
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [
-    PeopleCardContainerComponent,
-    PeopleCardComponent
-  ],
-  exports: [
-    PeopleCardContainerComponent
-  ],
+  imports: [CommonModule],
+  declarations: [PeopleCardContainerComponent, PeopleCardComponent],
+  exports: [PeopleCardContainerComponent],
 })
-export class PeopleCardsModule { }
+export class PeopleCardsModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faSquare,
+      faCheckSquare,
+      farSquare,
+      farCheckSquare,
+      faStackOverflow,
+      faGithub,
+      faMedium
+    );
+  }
+}
