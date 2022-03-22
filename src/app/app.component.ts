aa1f6a9fc534cc699301b776324c4b3dd970ecdb
nkunic <natasa.kunic@gmail.com>
2022-03-22 18:59:17 +0100

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  faCoffee = faCoffee;
+}
--- src/app/people-cards/components/people-card/people-card.component.ts
+++ src/app/people-cards/components/people-card/people-card.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CardModel } from '../../model/card.model';
 
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-
 @Component({
   selector: 'app-people-card',
   templateUrl: './people-card.component.html',
@@ -11,6 +9,4 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 export class PeopleCardComponent {
   @Input() card: CardModel;
   @Input() imgRatio: '1-1' | '16-9' = '1-1';
-
-  faCoffee = faCoffee;
 }
