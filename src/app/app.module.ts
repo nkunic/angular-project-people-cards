8fa6217ac386d344aa7b548594e2b6700439c012
nkunic <natasa.kunic@gmail.com>
2022-03-22 18:32:06 +0100

    Font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { PeopleCardsModule } from './people-cards/people-cards.module';
-
 import { AppComponent } from './app.component';
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSquare as farSquare,
+  faCheckSquare as farCheckSquare,
+} from '@fortawesome/free-regular-svg-icons';
+import {
+  faStackOverflow,
+  faGithub,
+  faMedium,
+} from '@fortawesome/free-brands-svg-icons';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, PeopleCardsModule],
+  imports: [BrowserModule, FormsModule, PeopleCardsModule, FontAwesomeModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faSquare,
+      faCheckSquare,
+      farSquare,
+      farCheckSquare,
+      faStackOverflow,
+      faGithub,
+      faMedium
+    );
+  }
+}
--- src/app/people-cards/components/people-card/people-card.component.ts
+++ src/app/people-cards/components/people-card/people-card.component.ts
@@ -4,11 +4,9 @@ import { CardModel } from '../../model/card.model';
 @Component({
   selector: 'app-people-card',
   templateUrl: './people-card.component.html',
-  styleUrls: ['./people-card.component.scss']
+  styleUrls: ['./people-card.component.scss'],
 })
 export class PeopleCardComponent {
-
   @Input() card: CardModel;
   @Input() imgRatio: '1-1' | '16-9' = '1-1';
-
 }
