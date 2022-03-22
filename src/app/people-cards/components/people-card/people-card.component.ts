e862d798e268d2c4acaa16e50532b3f638356f92
nkunic <natasa.kunic@gmail.com>
2022-03-22 18:52:42 +0100

    3 files modified

--- src/app/people-cards/components/people-card/people-card.component.ts
+++ src/app/people-cards/components/people-card/people-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { CardModel } from '../../model/card.model';
 
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-people-card',
   templateUrl: './people-card.component.html',
@@ -9,4 +11,6 @@ import { CardModel } from '../../model/card.model';
 export class PeopleCardComponent {
   @Input() card: CardModel;
   @Input() imgRatio: '1-1' | '16-9' = '1-1';
+
+  faCoffee = faCoffee;
 }
--- src/app/people-cards/people-cards.module.ts
+++ src/app/people-cards/people-cards.module.ts
@@ -3,33 +3,9 @@ import { CommonModule } from '@angular/common';
 import { PeopleCardContainerComponent } from './container/people-card-container.component';
 import { PeopleCardComponent } from './components/people-card/people-card.component';
 
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
-import {
-  faSquare as farSquare,
-  faCheckSquare as farCheckSquare,
-} from '@fortawesome/free-regular-svg-icons';
-import {
-  faStackOverflow,
-  faGithub,
-  faMedium,
-} from '@fortawesome/free-brands-svg-icons';
-
 @NgModule({
   imports: [CommonModule],
   declarations: [PeopleCardContainerComponent, PeopleCardComponent],
   exports: [PeopleCardContainerComponent],
 })
-export class PeopleCardsModule {
-  constructor(library: FaIconLibrary) {
-    library.addIcons(
-      faSquare,
-      faCheckSquare,
-      farSquare,
-      farCheckSquare,
-      faStackOverflow,
-      faGithub,
-      faMedium
-    );
-  }
-}
+export class PeopleCardsModule {}
