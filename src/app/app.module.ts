7d1f8ed9e6c863fd72369713f0f43bdc885d9df5
nkunic <natasa.kunic@gmail.com>
2022-03-22 22:59:15 +0100

    Font awesome fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { PeopleCardsModule } from './people-cards/people-cards.module';
 import { AppComponent } from './app.component';
-import {
-  FontAwesomeModule,
-  FaIconLibrary,
-} from '@fortawesome/angular-fontawesome';
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, PeopleCardsModule, FontAwesomeModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-  constructor(library: FaIconLibrary) {
-    // Add an icon to the library for convenient access in other components
-    library.addIcons(faAngleRight);
-  }
-}
+export class AppModule {}
