e22f5bf139792bb5ef9d0f436b9077c672fc577e
nkunic <natasa.kunic@gmail.com>
2022-03-22 19:13:31 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  faCoffee = faCoffee;
-}
+export class AppComponent {}
